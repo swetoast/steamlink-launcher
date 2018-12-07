a50cc62a90c32ffa569234948568bd7620de8db2
Toast <swetoast@users.noreply.github.com>
2018-12-07 14:12:32 +0100

    Update addon.py
    whoops bash functions was reversed

--- plugin.program.steamlink/addon.py
+++ plugin.program.steamlink/addon.py
@@ -28,6 +28,16 @@ def create_files():
     """Creates bash files to be used for this plugin."""
     with open('/tmp/steamlink-launcher.sh', 'w') as outfile:
         outfile.write('#!/bin/bash\n'
+                      'sudo openvt -c 7 -s -f clear\n'
+                      'sudo su osmc -c "sh /tmp/steamlink_watchdog.sh &" &\n'
+                      'sudo chown osmc:osmc /usr/bin/steamlink\n'
+                      'sudo su osmc -c "nohup openvt -c 7 -f -s steamlink >/dev/null 2>&1 &" &\n'
+                      'sudo openvt -c 7 -s -f clear\n'
+                      'sleep 0.5; sudo su -c "systemctl stop mediacenter &" &\n'
+                      'exit')
+        outfile.close()
+    with open('/tmp/steamlink-watchdog.sh', 'w') as outfile:
+        outfile.write('#!/bin/bash\n'
                       'if [ "$HYPERIONFIX" = 1 ]; then if [ "$(pgrep hyperion)" ]; '
                       'then sudo service hyperion stop; fi; fi\n'
                       'sleep 8\n'
@@ -39,16 +49,6 @@ def create_files():
                       'exit\n'
                       'else sleep 2; fi\n'
                       'done\n'
-                      'exit')
-        outfile.close()
-    with open('/tmp/steamlink-watchdog.sh', 'w') as outfile:
-        outfile.write('#!/bin/bash\n'
-                      'sudo openvt -c 7 -s -f clear\n'
-                      'sudo su osmc -c "sh /tmp/steamlink_watchdog.sh &" &\n'
-                      'sudo chown osmc:osmc /usr/bin/steamlink\n'
-                      'sudo su osmc -c "nohup openvt -c 7 -f -s steamlink >/dev/null 2>&1 &" &\n'
-                      'sudo openvt -c 7 -s -f clear\n'
-                      'sleep 0.5; sudo su -c "systemctl stop mediacenter &" &\n'
                       'exit')
         outfile.close()
 main()
