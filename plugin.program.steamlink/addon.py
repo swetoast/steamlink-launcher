60672233234a439c3ead01c39737e60e5202d7ac
Toast <swetoast@users.noreply.github.com>
2019-09-22 09:44:10 +0200

    Update addon.py

--- plugin.program.steamlink/addon.py
+++ plugin.program.steamlink/addon.py
@@ -9,7 +9,7 @@ __author__ = "toast"
 __url__ = "https://github.com/swetoast/steamlink-launcher/"
 __git_url__ = "https://github.com/swetoast/steamlink-launcher/"
 __credits__ = "toast"
-__version__ = "0.0.3"
+__version__ = "0.0.5"
 
 dialog = xbmcgui.Dialog()
 addon = xbmcaddon.Addon(id='plugin.program.steamlink')
