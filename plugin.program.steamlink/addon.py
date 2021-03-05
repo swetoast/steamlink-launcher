7bd4ef6d56061f7278465efab443a776b5c436fe
Toast <swetoast@users.noreply.github.com>
2021-03-05 13:51:02 +0100

    Update addon.py

--- plugin.program.steamlink/addon.py
+++ plugin.program.steamlink/addon.py
@@ -9,7 +9,7 @@ __author__ = "toast"
 __url__ = "https://github.com/swetoast/steamlink-launcher/"
 __git_url__ = "https://github.com/swetoast/steamlink-launcher/"
 __credits__ = "toast"
-__version__ = "0.0.7"
+__version__ = "0.0.7a"
 
 dialog = xbmcgui.Dialog()
 addon = xbmcaddon.Addon(id='plugin.program.steamlink')
@@ -31,31 +31,19 @@ sudo su -c "nohup sudo openvt -c 7 -s -f -l /tmp/steamlink-watchdog.sh >/dev/nul
         outfile.close()
     with open('/tmp/steamlink-watchdog.sh', 'w') as outfile:
         outfile.write("""#!/bin/bash
-# Shut down the graphical interface
 systemctl stop mediacenter
+if [ "$(systemctl is-active hyperion.service)" = "active" ]; then systemctl restart hyperion; fi
 
-# Restart hyperion service
-if [ "$HYPERIONFIX" = 1 ]; then
-    if [ "$(pgrep hyperion)" ]; then
-        service hyperion stop
-    fi
-    sleep 8
-    if [ ! "$(pgrep hyperion)" ]; then 
-        service hyperion start
-    fi
-fi
-
-# Install and run Steam Link
 if [ "$(which steamlink)" = "" ]; then
     curl -o /tmp/steamlink.deb -#Of http://media.steampowered.com/steamlink/rpi/steamlink.deb
     dpkg -i /tmp/steamlink.deb
     rm -f /tmp/steamlink.deb
 fi
 
-if [ $(/usr/bin/dpkg-query '-W' -f='${Status}' wakeonlan 2>/dev/null | /bin/grep -c "ok installed") -eq 1 ]; then 
-   if [ -f "$HOME/.wakeup" ]; then 
-      /usr/bin/wakeonlan $(cat "$HOME/.wakeup") # create a .wakeup with your MAC id in home folder to wake up your computer
-   fi
+if [ "$(/usr/bin/dpkg-query '-W' -f='${Status}' wakeonlan 2>/dev/null | /bin/grep -c "ok installed")" -eq 1 ]; then 
+   if [ -f "$HOME/.wakeup" ] 
+   then /usr/bin/wakeonlan "$(cat "$HOME/.wakeup")"
+   else sudo apt install wakeonlan -y;  /usr/bin/wakeonlan "$(cat "$HOME/.wakeup")"; fi
 fi
 
 sudo -u osmc steamlink
